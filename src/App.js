17337c72b7332b30209c21552598605d7d47f9d5
Nirakar Jena <jenashubham60@gmail.com>
2023-07-23 22:54:44 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./index.css"
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Projects from "./routes/Projects";
+
+import {Route , Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+    </Routes>
+    </>
   );
 }
 
