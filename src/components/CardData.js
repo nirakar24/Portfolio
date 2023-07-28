8fac9d691e0ff9415bb554603ce2a4502d1a67a3
Nirakar Jena <jenashubham60@gmail.com>
2023-07-28 18:18:25 +0530

    Projects

--- src/components/CardData.js
+++ src/components/CardData.js
@@ -1,33 +1,44 @@
-import project1 from "../assets/contact.jpg"
+import pixio from "../assets/pixio.png"
+import sound from "../assets/Sound.png"
+import sound2 from "../assets/sound2.png"
+import media from "../assets/media.png"
+import port from "../assets/port.png"
 
 const ProjectCardData =[
     {
-        imgsrc: project1,
-        title:"Pixio",
-        text:"Project description",
-        view:"url.com",
-        source:"https://github.com/nirakar24"
+        imgsrc: pixio,
+        title:"Pixio - Image Editing Application (Python)",
+        text:"Pixio is an image editing application developed using Python and the OpenCV library. It offers a wide range of image manipulation features, providing users with the ability to enhance, transform, and creatively edit their images.",
+        view:"https://github.com/nirakar24/Pixio",
+        source:"https://github.com/nirakar24/Pixio"
     },
     {
-        imgsrc: project1,
-        title:"Pixio2",
-        text:"Project description2",
-        view:"url.com",
-        source:"https://github.com/nirakar24"
+        imgsrc: sound,
+        title:"Sound Oasis - Music Streaming Website(Django)",
+        text:"Sound Oasis is a dynamic music streaming website built using Django. It enables users to discover and enjoy their favorite music through personalized playlists and intelligent song recommendations, creating an immersive music streaming experience.",
+        view:"https://github.com/nirakar24/Music_streaming_website",
+        source:"https://github.com/nirakar24/Music_streaming_website"
     },
     {
-        imgsrc: project1,
-        title:"Pixio3",
-        text:"Project description3",
-        view:"url.com",
-        source:"https://github.com/nirakar24"
+        imgsrc: sound2,
+        title:"Sound - Local MP3 Player (Python)",
+        text:"Sound is a user-friendly local MP3 player developed in Python with a Tkinter-based GUI. It allows users to play and manage their MP3 audio files effortlessly, providing an intuitive and enjoyable music playback experience.",
+        view:"https://github.com/nirakar24/Sound",
+        source:"https://github.com/nirakar24/Sound"
     },
     {
-        imgsrc: project1,
-        title:"Pixio4",
-        text:"Project description3",
-        view:"https://github.com/nirakar24",
-        source:"https://github.com/nirakar24",
+        imgsrc: media,
+        title:"Media Player (Java)",
+        text:"The Media Player application, developed in Java, allows users to play any local video or audio files in MP4 and MP3 formats, respectively. It provides a simple yet effective media playback solution with an easy-to-use interface.",
+        view:"https://github.com/nirakar24/MediaPlayer",
+        source:"https://github.com/nirakar24/MediaPlayer",
+    },
+    {
+        imgsrc: port,
+        title:"Portfolio Website (React.js)",
+        text:"My personal portfolio website is developed using React.js. It showcases my projects, skills, and experiences as a Python Developer and tech enthusiast. The website provides an overview of my journey in the tech industry and highlights my proficiency in software development and web technologies.",
+        view:"https://takemitchy.vercel.app",
+        source:"https://github.com/nirakar24/Portfolio",
     }
 ]
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import './FooterStyles.css'
 
 import React from 'react'
-import { FaGithub, FaHome, FaInstagram, FaLinkedin, FaMailBulk, FaPhone } from 'react-icons/fa'
+import { FaGithub, FaHackerrank, FaHome, FaInstagram, FaLinkedin, FaMailBulk, FaPhone } from 'react-icons/fa'
+import {Link} from "react-router-dom"
 
 const Footer = () => {
   return (
@@ -17,17 +18,18 @@ const Footer = () => {
                    +91-7400285472</h4>
                 </div>
                 <div className='email'>
-                   <h4><FaMailBulk size={20} style={{color:"#fff", marginRight:"2rem"}}/>
-                   jenashubham60@gmail.com</h4>
+                 <a href='mailto:jenashubham60@gmail.com'><h4><FaMailBulk size={20} style={{color:"#fff", marginRight:"2rem"}}/>
+                   jenashubham60@gmail.com</h4></a>
                 </div>  
             </div>
             <div className='right'>
               <h4>Who am I?</h4>
               <p>Dedicated and motivated engineering student pursuing a Bachelor's degree in Artificial Intelligence and Data Science. Skilled in Python and Java programming languages, with hands-on experience in software development, image processing, and web technologies. Eager to leverage knowledge and passion for AI to contribute to innovative projects and further professional growth.</p>
               <div className='social'>
-              <FaGithub size={20} style={{color:"#fff", marginRight:"2rem"}}/>
-              <FaLinkedin size={20} style={{color:"#fff", marginRight:"2rem"}}/>
-              <FaInstagram size={20} style={{color:"#fff", marginRight:"2rem"}}/>
+              <Link to="https://github.com/nirakar24"><FaGithub className='gith' size={20} style={{color:"#fff", marginRight:"2rem"}}/></Link>
+              <Link to="https://www.linkedin.com/in/nirakar-jena-ab12b518b/"><FaLinkedin className='linkd' size={20} style={{color:"#fff", marginRight:"2rem"}}/></Link>
+              <Link to="https://www.instagram.com/nirakar_24_/"><FaInstagram className='insta' size={20} style={{color:"#fff", marginRight:"2rem"}}/></Link>
+              <Link to="https://www.hackerrank.com/jenashubham60?hr_r=1"><FaHackerrank className='hacker' size={20} style={{color:"#fff", marginRight:"2rem"}}/></Link>
               </div>
             </div>
         </div>
